bb6bdafd3f66dd5f463d6a7182b2a15436fb1d0b
Jan Macku <jamacku@redhat.com>
2021-11-25 16:35:27 +0100

    WIP: run rename check every time

--- src/services/rename.pull.service.ts
+++ src/services/rename.pull.service.ts
@@ -2,71 +2,58 @@ import { Context } from "probot";
 
 export async function renamePullrequest(context: Context) {
     // TODO: Find proper way to do this ( ... as any ) !!!
-    const { payload, log }: any = context;
+    const { payload }: any = context;
     const { title } = payload.pull_request;
 
-    if (!isBuginTitle(title)) {
-        const bugRegex = /(^\s*|\n|\\n)(Resolves|Related): ?(#\d+)$/;
+    const bugRegex = /(^\s*|\n|\\n)(Resolves|Related): ?(#\d+)$/;
 
-        /* Get list of commits for given PR */
-        const commits = (await context.octokit.pulls.listCommits(
-            context.pullRequest()
-        )).data.map(commit => {
-            /* Transform recived object to contain only relevant informations */
-            const commitDesc = {
-                sha: commit.sha,
-                message: commit.commit.message
-            }
-
-            const result = commitDesc.message.match(bugRegex);
-            return {
-                ...commitDesc,
-                bugRef: Array.isArray(result) ? result[3] : ''
-            };
-        });
-
-        let bug: string = '';
-        let invalidCommits = commits.filter(commit => {
-            if (commit.bugRef && bug.length && commit.bugRef === bug) {
-                return false; // Already noted bug reference
-            } else if (commit.bugRef && !bug.length) {
-                bug = commit.bugRef;
-                return false; // First bug reference
-            } else {
-                return true // Multiple bug references in one PR or no bug reference
-            }
-        });
-
-        log(invalidCommits);
-        log(bug);
-
-        if (bug) {
-            context.octokit.pulls.update(
-                context.pullRequest({
-                    title: `(${bug}) ${title}`
-                })
-            );
+    /* Get list of commits for given PR */
+    const commits = (await context.octokit.pulls.listCommits(
+        context.pullRequest()
+    )).data.map(commit => {
+        /* Transform recived object to contain only relevant informations */
+        const commitDesc = {
+            sha: commit.sha,
+            message: commit.commit.message
         }
 
-        if (invalidCommits.length) {
-            const reviewLog = (await context.octokit.pulls.createReview(
-                context.pullRequest({
-                    event: 'COMMENT',
-                    body: 'Some invalid bug references!!!'
-                })
-            )).data;
-            console.log(reviewLog);
+        const result = commitDesc.message.match(bugRegex);
+        return {
+            ...commitDesc,
+            bugRef: Array.isArray(result) ? result[3] : ''
+        };
+    });
+
+    let bug: string = '';
+    let invalidCommits = commits.filter(commit => {
+        if (commit.bugRef && bug.length && commit.bugRef === bug) {
+            return false; // Already noted bug reference
+        } else if (commit.bugRef && !bug.length) {
+            bug = commit.bugRef;
+            return false; // First bug reference
+        } else {
+            return true // Multiple bug references in one PR or no bug reference
         }
+    });
 
-        
-
-    } else {
-        log('It contains bug reference', title);
+    if (bug) {
+        context.octokit.pulls.update(
+            context.pullRequest({
+                title: `(${bug}) ${title}`
+            })
+        );
     }
 
+    // TODO mark review by bot to metadata! so It wouldn't spam on PRs
+    if (invalidCommits.length) {
+        context.octokit.pulls.createReview(
+            context.pullRequest({
+                event: 'COMMENT',
+                body: 'Some invalid bug references!!!'
+            })
+        );
+    }
 }
-
-
 
 export function isBuginTitle(title: string) {
     /* Regex '/^\s*\(\#?\d+\)/' check if PR title starts with bug reference e.g. (#123456) or (123456) */
