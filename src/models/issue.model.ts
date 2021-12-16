945cae6eee2ff62d3a6fbec9a400d5f6b76d2e1e
Jan Macku <jamacku@redhat.com>
2021-12-16 09:04:02 +0100

    Small tweeks
    * disable cspell check in CI
    * add script for running jest for TDD

--- src/models/issue.model.ts
+++ src/models/issue.model.ts
@@ -6,7 +6,18 @@ type Title = {
 export class Issue {
   constructor(
     protected readonly id: number,
-    protected _title: Title,
-    protected _body: string
+    protected title: Title,
+    protected body: string,
+    protected assignee?: string[],
+    protected milestone?: string,
+    protected project?: string[]
   ) {}
+
+  getTitle() {
+    return this.title;
+  }
+
+  setTitle(this: Issue, newTitle: Title) {
+    this.title = newTitle;
+  }
 }
--- src/models/pullRequest.model.ts
+++ src/models/pullRequest.model.ts
@@ -1,25 +1,27 @@
 import { Issue } from './issue.model';
-import { Commit } from './commit.model';
+// import { Commit } from './commit.model';
 
 export class PullRequest extends Issue {
+  // private bugRef: number[];
+
   constructor(
     id: number,
     title: {
       _name: string;
       _bugRef?: number;
     },
-    body: string,
-    private commits: Commit[]
+    body: string
+    // private commits: Commit[]
   ) {
     super(id, title, body);
     // should call regex and set bugRef and title
   }
 
-  set title(newTitle: string) {
-    this._title._name = newTitle;
-  }
+  // set title(newTitle: string) {
+  //   this._title._name = newTitle;
+  // }
 
-  get title() {
-    return `(#${this._title._bugRef}) ${this._title._name}`;
-  }
+  // get title() {
+  //   return `(#${this._title._bugRef}) ${this._title._name}`;
+  // }
 }
