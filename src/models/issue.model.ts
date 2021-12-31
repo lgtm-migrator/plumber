3825d6d3c1c52fe76dc17c7d07c19acbb56e4762
Jan Macku <jamacku@redhat.com>
2021-12-31 12:45:33 +0100

    Update issue model

--- src/models/issue.model.ts
+++ src/models/issue.model.ts
@@ -1,23 +1,51 @@
-type Title = {
-  _name: string;
-  _bugRef?: number;
+export type Title = {
+  name: string;
+  bugRef?: number;
+};
+
+export type IssueObject = {
+  id: number;
+  title: Title;
+  body: string;
+  assignee?: string[];
+  milestone?: string;
+  project?: string[];
 };
 
 export class Issue {
-  constructor(
-    protected readonly id: number,
-    protected title: Title,
-    protected body: string,
-    protected assignee?: string[],
-    protected milestone?: string,
-    protected project?: string[]
-  ) {}
+  protected readonly id: number;
+  protected _title: Title;
+  protected _body: string;
+  protected _assignee?: string[];
+  protected _milestone?: string;
+  protected _project?: string[];
 
-  getTitle() {
-    return this.title;
+  constructor(data: IssueObject) {
+    this.id = data.id;
+    this._title = data.title;
+    this._body = data.body;
+    this._assignee = data?.assignee;
+    this._milestone = data?.milestone;
+    this._project = data?.project;
   }
 
-  setTitle(this: Issue, newTitle: Title) {
-    this.title = newTitle;
+  get title() {
+    return this._title;
+  }
+
+  get titleString() {
+    return `(#${this.title.bugRef}) ${this.title.name}`;
+  }
+
+  get bugRef() {
+    return this.title.bugRef;
+  }
+
+  set title(newTitle: Title) {
+    this._title = newTitle;
+  }
+
+  set bugRef(bug: number | undefined) {
+    this._title.bugRef = bug;
   }
 }
