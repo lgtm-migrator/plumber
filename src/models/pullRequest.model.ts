d825ab4ed44613c3d735ee9f267ab24a25c07ff6
Jan Macku <jamacku@redhat.com>
2021-12-31 13:33:59 +0100

    pullRequest: Add commits to constructor

--- src/models/pullRequest.model.ts
+++ src/models/pullRequest.model.ts
@@ -1,22 +1,12 @@
 import { IssueObject } from '../types/issue';
 import { Issue } from './issue.model';
-// import { Commit } from './commit.model';
+import { Commit } from './commit.model';
 
 export class PullRequest extends Issue {
-  // private bugRef: number[];
+  protected _commits: Commit[];
 
-  constructor(
-    data: IssueObject // private commits: Commit[]
-  ) {
+  constructor(data: IssueObject, commits: Commit[]) {
     super(data);
-    // should call regex and set bugRef and title
+    this._commits = commits;
   }
-
-  // set title(newTitle: string) {
-  //   this._title._name = newTitle;
-  // }
-
-  // get title() {
-  //   return `(#${this._title._bugRef}) ${this._title._name}`;
-  // }
 }
