03d756ca108bb259fa3236bba7cdd0a89b45d175
Jan Macku <jamacku@redhat.com>
2022-01-01 11:16:56 +0100

    models: Use PullRequestObject

--- src/models/pullRequest.model.ts
+++ src/models/pullRequest.model.ts
@@ -1,12 +1,25 @@
-import { IssueObject } from '../types/issue';
 import { Issue } from './issue.model';
 import { Commit } from './commit.model';
 
+// import { BugRef } from '../types/commit';
+import { PullRequestObject } from '../types/pullRequest';
 export class PullRequest extends Issue {
   protected _commits: Commit[];
 
-  constructor(data: IssueObject, commits: Commit[]) {
+  constructor(data: PullRequestObject) {
     super(data);
-    this._commits = commits;
+    this._commits = data.commits;
   }
+
+  // private getBugRef(): BugRef {
+  //   const bugRefs = this.getBugRefs();
+  // }
+
+  // private getBugRefs(): BugRef[] {
+  //   const bugRefs = this._commits.map(commit => {
+  //     return commit.bugRef;
+  //   });
+
+  //   return bugRefs;
+  // }
 }
