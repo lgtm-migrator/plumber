100a4d58c9e38bcadb585ade4a44c1babc0e7e99
Jan Macku <jamacku@redhat.com>
2022-01-15 10:50:46 +0100

    clean code: get rid of useless comments

--- src/index.ts
+++ src/index.ts
@@ -12,37 +12,24 @@ import {
 } from './handlers/pull-request-checks.handler';
 
 export = (app: Probot) => {
-  /*
-   *  */
   app.on(plumberPullEvent.init, handlePullRequestInit.bind(null, app));
 
-  /*
-   * Title change */
   app.on(plumberPullEvent.edited, handlePullRequestUpdate.bind(null, app));
 
-  /*
-   *  */
   app.on(plumberPullEvent.labels, handlePullRequestLabels.bind(null, app));
 
-  /*
-   *  */
   app.on(plumberPullEvent.reviews, handlePullRequestReviews.bind(null, app));
 
-  /*
-   *  */
   app.on(
     plumberPullEvent.checksInProgress,
     handlePullRequestChecksInProgress.bind(null, app)
   );
 
-  /*
-   *  */
   app.on(
     plumberPullEvent.checksCompleted,
     handlePullRequestChecksCompleted.bind(null, app)
   );
 
-  /* Log errors */
   app.onError(async error => {
     app.log.error(error);
   });
