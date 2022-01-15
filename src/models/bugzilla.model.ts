be58c66fa4489b7b0826fcc1f570930ecde12acb
Jan Macku <jamacku@redhat.com>
2022-01-15 20:26:52 +0100

    models(bugzilla): remove unnecessary protected keyworkds

--- src/models/bugzilla.model.ts
+++ src/models/bugzilla.model.ts
@@ -1,13 +1,13 @@
 import BugzillaAPI from 'bugzilla';
 
 export abstract class Bugzilla {
-  protected readonly _api: BugzillaAPI;
+  readonly _api: BugzillaAPI;
 
   constructor() {
     this._api = new BugzillaAPI('https://bugzilla.mozilla.org', '<api key>');
   }
 
-  protected get bugzilla() {
+  get bugzilla() {
     return this._api;
   }
 }
