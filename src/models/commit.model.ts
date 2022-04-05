e67858e9ce9bab43f7b3e4813940f9769eefa2ff
Jan Macku <jamacku@redhat.com>
2022-04-05 10:14:53 +0200

    commit.model: Add upstreamRef and rhelOnly

--- src/models/commit.model.ts
+++ src/models/commit.model.ts
@@ -3,14 +3,20 @@ import { CommitObject, BugRef } from '../types/commit';
 export class Commit {
   private readonly _sha: string;
   private readonly _message: string;
-  private readonly _title: string | undefined;
+
+  private readonly _title?: string;
   private readonly _bugRef: BugRef;
+  private readonly _upstreamRef?: string;
+  private readonly _rhelOnly?: boolean;
 
   constructor(data: CommitObject) {
     this._sha = data.sha;
     this._message = data.message;
+
     this._title = data?.title ?? this.getTitle(this.message);
     this._bugRef = data?.bugRef ?? this.getBugRef(this.message);
+    this._upstreamRef = data?.upstreamRef ?? this.getUpstreamRef(this.message);
+    this._rhelOnly = data?.rhelOnly ?? this.getRhelOnly(this.message);
   }
 
   get sha() {
@@ -29,17 +35,40 @@ export class Commit {
     return this._bugRef;
   }
 
-  private getTitle(message: string): string {
+  get upstreamRef() {
+    return this._upstreamRef;
+  }
+
+  get rhelOnly() {
+    return this._rhelOnly;
+  }
+
+  private getTitle(message: string) {
     const TitleSize = 70;
     let slicedMsg = message.split(/\n/, 1)[0].slice(0, TitleSize);
 
     return slicedMsg.length < TitleSize ? slicedMsg : `${slicedMsg}...`;
   }
 
-  private getBugRef(message: string): BugRef {
+  private getBugRef(message: string) {
     const bugRegex = /(^\s*|\n|\\n)(Resolves|Related): ?#(\d+)$/;
 
     const bugRef = message.match(bugRegex);
     return Array.isArray(bugRef) ? +bugRef[3] : undefined;
+  }
+
+  private getUpstreamRef(message: string) {
+    const upstreamRegex =
+      /^\(cherry picked from commit (\b[0-9a-f]{5,40}\b)\)$/;
+
+    const upstreamRef = message.match(upstreamRegex);
+    return Array.isArray(upstreamRef) ? upstreamRef[1] : undefined;
+  }
+
+  private getRhelOnly(message: string) {
+    const rhelOnlyRegex = /^RHEL-only$/;
+
+    const rhelOnly = message.match(rhelOnlyRegex);
+    return Array.isArray(rhelOnly) ? rhelOnly[0] !== '' : false;
   }
 }
--- src/types/commit.d.ts
+++ src/types/commit.d.ts
@@ -3,6 +3,8 @@ export type CommitObject = {
   message: string;
   title?: string;
   bugRef?: BugRef;
+  upstreamRef?: string;
+  rhelOnly?: boolean;
 };
 
 export type BugRef = number | undefined;
